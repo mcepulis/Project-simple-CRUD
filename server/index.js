dc6b13322f0c0ba3afa811933f44b8af178d49cf
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-04-22 12:50:45 +0300

    CRUD from backend side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,55 @@
-console.log(5);
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/test', (req, res) => {
+    res.send("Veikia");
+});
+
+let tasks = [
+    { id: 1, title: 'Task 1' },
+    { id: 2, title: 'Task 2' },
+  ];
+
+  //view all tasks
+  app.get('/api/tasks', (req, res) => {
+    res.json(tasks);
+  });
+
+//add task
+app.post('/api/tasks', (req, res) => {
+    const { title } = req.body;
+    const id = tasks.length + 1;
+    const newTask = { id, title };
+    tasks.push(newTask);
+    res.status(201).json(tasks);
+})
+
+//update task
+
+app.put('/api/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    const task = tasks.find(task => task.id === parseInt(id));
+    if (!task) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+    task.title = title;
+    res.json(tasks);
+});
+
+//delete task
+app.delete('/api/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    tasks = tasks.filter(task => task.id === parseInt(id));
+    res.status(204).end;
+});
+
+
+
+app.listen(5559, () => {
+    console.log('http://localhost:5559');
+})
